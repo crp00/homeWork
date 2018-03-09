c8e4dab7aa8e3a1b6fae66f2829069b686a3e7b3
Alex Demyanchuk <alexd3m@gmail.com>
2018-03-09 11:43:43 +0200

    test commit

---
+++ homeWork/homeWorkOne/homeWorkOne/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace homeWorkOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your name here: ");
+        }
+    }
+}
