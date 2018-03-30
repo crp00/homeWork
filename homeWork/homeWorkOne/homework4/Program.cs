3de71c6ae74986b40dac72eacedc9b6e4dc76445
Alex Demyanchuk <alexd3m@gmail.com>
2018-03-30 18:21:57 +0300

    homework #4 in progress

---
+++ homeWork/homeWorkOne/homework4/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+
+namespace homework4
+/* Homework task 1.1 & 1.2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] foodLowerCase = { "apple", "avocado", "banana", "apricot", "jackfruit", "cherimoya", "papaya", "pineapple", "lime", "lemon", "grape", "orange", "grapefruit", "strawberry" };
+            string[] foodHigherCase = { "Apple", "Avocado", "Banana", "Apricot", "Jackfruit", "Cherimoya", "Papaya", "Pineapple", "Lime", "Lemon", "Grape", "Orange", "Grapefruit", "Strawberry" };
+
+            Console.WriteLine("Task #1.1: \n");
+
+            foreach (string item in foodLowerCase)
+            {
+                if (item.Contains("ap"))
+                {
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine(item);
+                }
+            }
+
+            Console.WriteLine("\nTask #1.2: \n");
+
+            foreach (string item in foodHigherCase)
+            {
+                string newItem = item.ToLower();
+                if (newItem.Contains("ap"))
+                {
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
+//Homework task 2 & 3
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter int value: ");
+            int i;
+            int parsedInput;
+            var userInput = Console.ReadLine();
+
+            bool isParsed = Int32.TryParse(userInput, out parsedInput);
+            if (isParsed)
+            {
+                int sum = 0;
+
+                Console.WriteLine($"The number provided is: {parsedInput}");
+                
+                for (i = 1; i <= parsedInput; i++)
+                {
+                    Console.WriteLine(i);
+                    sum += i;
+                }
+                Console.WriteLine($"The sum of these numbers is: {sum}");
+            }
+            else
+            {
+                Console.WriteLine("Your value is not int..");
+            }
+        }
+    }
+}
+*/
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the color of banana?\n\ta. Red\n\tb. Green\n\tc. Yellow\n\td. Blue\n\te. Purple");
+            string userInput;
+
+            do
+            {
+                userInput = Console.ReadLine();
+                if (userInput == "c")
+                {
+                    Console.WriteLine("Correct answer");
+                }
+                else
+                {
+                    Console.WriteLine("Wrong answer, do you want to try again? Y/N");
+                    string userExit = Console.ReadLine();
+                    if (userExit == "Y")
+                    {
+                        Console.WriteLine("Please choose answer once more: ");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Terminating application");
+                        break;
+                    }
+                }
+            }
+            while (userInput != "c"); 
+        }
+    }
+}
+
+
+
+
