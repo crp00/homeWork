26196fb2bda8155b662d840227fb47391bedd90a
Alex Demyanchuk <alexd3m@gmail.com>
2018-04-04 14:57:43 +0300

    homework 5 tasks 1 & 2

---
+++ homeWork/homeWorkOne/homework5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace homework5
+{
+    class Program
+    {
+        static int[] someArray = { 4, 33, 32, 55, 34, 77 };
+
+        static void ReturnAll()
+        {
+            foreach (int x in someArray)
+            {
+                Console.WriteLine(x);
+            }
+        }
+        static void ReturnLast()
+        {
+            Console.WriteLine(someArray.Last());
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Task 1:");
+            ReturnAll();
+            Console.WriteLine("Task 2:");
+            ReturnLast();
+        }
+    }
+}
