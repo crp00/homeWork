1f865d9aa71cfb4e831f59cfedbfc64fd7b344e0
Alex Demyanchuk <alexd3m@gmail.com>
2018-04-11 16:28:02 +0300

    task 5 beginning

--- homeWork/homeWorkOne/homework5/Program.cs
+++ homeWork/homeWorkOne/homework5/Program.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 
+/*
 namespace homework5
 {
     class Program
     {
-        static int[] someArray = { 4, 33, 32, 55, 34, 77 };
+        static int[] someArray = { 4, 33, 32, 55, 34, 77, 13 };
 
         static void ReturnAll()
         {
@@ -25,6 +26,39 @@ namespace homework5
             ReturnAll();
             Console.WriteLine("Task 2:");
             ReturnLast();
+        }
+    }
+}
+*/
+
+namespace homework5
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var userInput = string.Empty;
+
+            do
+            {
+                Console.WriteLine("Please choose option to proceed:\na. LOG IN\nb. SIGN UP\nc. EXIT");
+                userInput = Console.ReadLine();
+                if (userInput == "a")
+                {
+                    Console.WriteLine("LOG IN GO GO");
+                }
+                else if (userInput == "b")
+                {
+                    Console.WriteLine("Sign Up GO GO");
+                }
+                else if (userInput == "c")
+                {
+                    Console.WriteLine("Exiting...");
+                    break;
+                }
+            }
+            while (true);
         }
     }
 }
