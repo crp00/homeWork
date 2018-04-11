8d835cb09396d0ccede20808e83ebe3f4ce381e5
Alex Demyanchuk <alexd3m@gmail.com>
2018-04-11 18:01:28 +0300

    updated

--- homeWork/homeWorkOne/homework5/Program.cs
+++ homeWork/homeWorkOne/homework5/Program.cs
@@ -35,18 +35,29 @@ namespace homework5
 {
     class Program
     {
-
         static void Main(string[] args)
         {
             var userInput = string.Empty;
+            var userLogin = string.Empty;
+            var userPassword = string.Empty;
 
             do
             {
                 Console.WriteLine("Please choose option to proceed:\na. LOG IN\nb. SIGN UP\nc. EXIT");
-                userInput = Console.ReadLine();
+                userInput = Console.ReadLine(); 
                 if (userInput == "a")
                 {
-                    Console.WriteLine("LOG IN GO GO");
+                    Console.WriteLine("Please enter your login:");
+                    userLogin = Console.ReadLine();
+                    Console.WriteLine("Please enter your password:");
+                    userPassword = Console.ReadLine();
+
+                    if (userList.ContainsKey(userLogin) && userList[userLogin] == userPassword)
+                    {
+                        Console.WriteLine("Authorized");
+                    }
+                    else
+                        Console.WriteLine("User not found");
                 }
                 else if (userInput == "b")
                 {
@@ -59,6 +70,21 @@ namespace homework5
                 }
             }
             while (true);
+        }
+        static Dictionary<string, string> userList = new Dictionary<string, string>
+        {
+            { "admin","12345"}
+        };
+        static Dictionary<string, string> itemList = new Dictionary<string, string>
+        {
+            { "1","Apple"},
+            { "2","Lemon"},
+            { "3","Grape"},
+            { "4","Peach"}
+        };
+        static void Shop()
+        {
+
         }
     }
 }
