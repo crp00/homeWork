c7b73b2d176988d711b74b3c446417ff38f06c54
Alex Demyanchuk <alexd3m@gmail.com>
2018-05-07 20:41:54 +0300

    rework

---
+++ homeWork/homeWorkOne/homework8Tests/DepartmentTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework8Tests
+{
+    using NUnit.Framework;
+    using System;
+    using homework8;
+
+    [TestFixture]
+
+    class DepartmentTests
+    {
+        [Test]
+        [Category("Department")]
+        public void Add_newEmployee_SaveToList()
+        {
+            //arrange
+            var someEmployee1 = new Employee("John", "Doe", 34);
+            //act
+            //assert
+        }
+
+        [Test]
+        [Category("Department")]
+        public void Add_EmptyEmployee_SaveToList()
+        {
+
+        }
+    }
+}
