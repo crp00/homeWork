00a2e6c1601867e3f89509bfb31b5d8d8bcb31db
Oleksii Demyanchuk <Oleksii_Demyanchuk@epam.com>
2018-07-10 16:51:52 +0300

    Added GetHelp() method

---
+++ homeWork/homeWorkOne/homework9/HelperClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace homework9
+{
+    class HelperClass
+    {
+        public void GetHelp()
+        {
+            //empty
+        }
+    }
+}
