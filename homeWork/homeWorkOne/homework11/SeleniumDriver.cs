8748c3e3d9787262c76f67a4592a2f97a328a2ae
Oleksii Demyanchuk <oleksii_demyanchuk@epam.com>
2018-07-25 19:33:39 +0300

    initial commit on selenium

---
+++ homeWork/homeWorkOne/homework11/SeleniumDriver.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace homework11
+{
+    public static class SeleniumDriver
+    {
+        private static IWebDriver _driver;
+
+        public static IWebDriver Driver
+        {
+            get
+            {
+                if (_driver == null)
+                {
+                    _driver = new ChromeDriver();
+                }
+                return _driver;
+            }
+
+            set { _driver = value; }
+        }
+    }
+}
---
+++ homeWork/homeWorkOne/homework11/Test1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace homework11
+{
+    [TestFixture]
+    public class Test1
+    {
+        [TestCase]
+        public void Sometest()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://www.citrus.ua/";
+
+            var searchElement = driver.FindElement(By.XPath("//input[@type='search' and @id='multisearch']"));
+            searchElement.SendKeys("macbook");
+            searchElement.Submit();
+
+            Assert.That(driver.Url.Contains("macbook"));
+
+            driver.Close();
+
+
+
+
+        }
+    }
+}
