395ccc028a11de3d163f7899c78aaca62186e750
Oleksii Demyanchuk <oleksii_demyanchuk@epam.com>
2018-07-31 20:16:39 +0300

    fail 4

--- homeWork/homeWorkOne/homework11/Test1.cs
+++ homeWork/homeWorkOne/homework11/Test1.cs
@@ -13,12 +13,12 @@ namespace homework11
     public class Test1
     {
         [TestCase]
-        public void Sometest()
+        public void MackBook_FromHomePageSearchMackbookAndValidateResults()
         {
             IWebDriver driver = new ChromeDriver();
             driver.Url = "https://www.citrus.ua/";
 
-            var searchKey = "macbook";
+            var searchKey = "MacBook";
             var searchElement = driver.FindElement(By.XPath("//input[@type='search' and @id='multisearch']"));
             searchElement.SendKeys(searchKey);
             searchElement.Submit();
@@ -28,20 +28,75 @@ namespace homework11
 
             System.Threading.Thread.Sleep(5000);
 
-            resultSet = driver.FindElements(By.XPath("//div[@class='title-itm']/h5")).ToList();
+            resultSet = driver.FindElements(By.XPath("//div[@class='roww catalog-roww']//a/div/h5")).ToList();
 
-            //var resultSet = driver.FindElements(By.XPath("//div[@class='title-itm']/h5")).ToString().ToList();
+            foreach (IWebElement x in resultSet)
+            {
+                titleNames.Add(x.Text);
+            }
 
-            //Assert.That(resultSet.Contains(searchKey));
+            Assert.IsTrue(titleNames.All(i => i.Contains(searchKey)));
 
             driver.Quit();
+        }
 
+        [TestCase]
+        public void LG_FromHomePageGoToTVapplyLGfacetAndValidateElelement()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://www.citrus.ua/";
 
+            var firstElement = driver.FindElement(By.XPath("//a[@title='TV']"));
 
+            firstElement.Click();
 
+            System.Threading.Thread.Sleep(5000);
 
+            var secondElement = driver.FindElement(By.XPath("//*[@id='__layout']/div/main/div/div[2]/aside/div/div[3]/ul/li[4]/label/span[1]")); //will refactor later
 
+            secondElement.Click();
 
+            var thirdElement = driver.FindElement(By.XPath("//span[@title='Бренд']"));
+
+            var elementValue = thirdElement.Text;
+
+            Assert.That(elementValue.Contains("LG"));
+
+            driver.Quit();
+        }
+
+        [TestCase]
+        public void PRDNAME_GoToTVandSaveFirstProductThenTRATATATA()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://www.citrus.ua/";
+
+            var firstElement = driver.FindElement(By.XPath("//a[@title='TV']"));
+
+            firstElement.Click();
+
+            System.Threading.Thread.Sleep(5000);
+
+            var productPriceElement1 = driver.FindElement(By.XPath("//*[@id='__layout']/div/main/div/div[2]/section/div[2]/div/div[1]/div/div/div[1]/div[4]/span[2]/span[1]")); //will refactor later
+            var productNameElement1 = driver.FindElement(By.XPath("//*[@id='__layout']/div/main/div/div[2]/section/div[2]/div/div[1]/div/div/div[1]/a/div/h5"));
+
+            var productPricePRE = productPriceElement1.Text;
+            var productNamePRE = productNameElement1.Text;
+
+            var secondElement = driver.FindElement(By.XPath("//*[@id='__layout']/div/main/div/div[2]/section/div[2]/div/div[1]/div/div/div[1]/a"));
+            secondElement.Click();
+
+            System.Threading.Thread.Sleep(5000);
+
+            var productPriceElement2 = driver.FindElement(By.XPath("//*[@id='__layout']/div/main/div[2]/div[2]/section/div/div[2]/div/div[2]/div[1]/div[1]/a/span[1]/span[1]"));
+            var productNameElement2 = driver.FindElement(By.XPath("//*[@id='__layout']/div/main/div[2]/h1"));
+
+            var productPricePOST = productPriceElement2.Text;
+            var productNamePOST = productNameElement2.Text;
+
+            Assert.That(productPricePRE.Equals(productPricePOST) && productNamePRE.Equals(productNamePOST));
+
+            driver.Quit();
         }
     }
 }
