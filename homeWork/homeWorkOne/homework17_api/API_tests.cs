177d37496cdb43bb45a0f408850bfab3bc2b7850
Oleksii Demyanchuk <oleksii_demyanchuk@epam.com>
2018-11-05 17:01:17 +0200

    something

---
+++ homeWork/homeWorkOne/homework17_api/API_tests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using NUnit.Framework;
+
+namespace homework17_api
+{
+    [TestFixture]
+    class API_tests
+    {
+        private HttpClient client = new HttpClient();
+
+        [SetUp]
+        public void Setup()
+        {
+           //
+        }
+
+        [TearDown] 
+        public void Teardown()
+        {
+            //
+        }
+
+        [Test]
+        public void test1()
+        {
+            async void getResponse()
+            {
+                var uri = new Uri("http://epuakyiw1888.kyiv.epam.com:8024/api/items");
+                var result = await client.GetStringAsync(uri);
+
+                Console.WriteLine(result);
+            }
+
+            getResponse();
+        }
+    }
+}
--- homeWork/homeWorkOne/homework17_api/Program.cs
+++ homeWork/homeWorkOne/homework17_api/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net.Http;
+using System.IO;
 
 namespace homework17_api
 {
@@ -11,6 +12,37 @@ namespace homework17_api
     {
         static void Main(string[] args)
         {
+            //string url = "http://epuakyiw1888.kyiv.epam.com:8024/api/items";
+
+            //var newRequest = WebRequest.Create(url);
+
+            //Stream someStream = newRequest.GetResponse().GetResponseStream();
+
+            //StreamReader someReader = new StreamReader(someStream);
+
+            //string sLine = "";
+            //int i = 0;
+
+            //while (sLine != null)
+            //{
+            //    i++;
+            //    sLine = someReader.ReadLine();
+            //    if (sLine != null)
+            //    {
+            //        Console.WriteLine("{0}:{1}", i, sLine);
+            //    }
+            //    Console.ReadLine();
+
+            async void checkResponse()
+            {
+                var uri = new Uri("http://epuakyiw1888.kyiv.epam.com:8024/api/items");
+                var hc = new HttpClient();
+
+                var result = await hc.GetStringAsync(uri);
+                Console.WriteLine(result);
+            }
+
+            checkResponse();
         }
     }
 }
